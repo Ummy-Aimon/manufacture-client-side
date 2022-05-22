2ba02b0f087b82406f372b8213a1ff6caf228380
Ummy-Aimon <ummyatia@gmail.com>
2022-05-22 21:53:42 +0600

    contact added

---
+++ src/Component/Pages/Contact/Contact.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import { Button, FloatingLabel, Form } from 'react-bootstrap';
+import {AiOutlineArrowRight} from 'react-icons/ai';
+import { Link } from 'react-router-dom';
+import contact from "../../../image/contact-1.jpg"
+import './Contact.css'
+
+const Contact = () => {
+    return (
+        <div className="contact">
+            <div  className="title-text">
+              <h1 className="title">Get In Touch</h1>
+              <p className="fw-bold text-warning">Don't Hesited To Conatct With Roxce</p>
+              </div>
+              <div className="container">
+                  <div className="row">
+                      <div className="col-md-6">
+              <Form className="mt-4">
+              <Form.Group className="mb-3" controlId="formBasicPassword">
+    <Form.Control type="text" placeholder="Enter Your Name" />
+  </Form.Group>
+  <Form.Group className="mb-3" controlId="formBasicEmail">
+    <Form.Control type="email" placeholder="Enter Your email" />
+  </Form.Group>
+  <FloatingLabel controlId="floatingTextarea2" label="Your Message">
+    <Form.Control
+      as="textarea"
+      placeholder="Your Message"
+      style={{ height: '100px' }}
+    />
+    <br></br>
+  </FloatingLabel>
+  <Button as={Link}  to="/gallary" variant="warning" type="submit">
+    Send Message <AiOutlineArrowRight></AiOutlineArrowRight>
+  </Button>
+</Form>
+<br></br>
+</div>
+<div className="col-md-6">
+    <img src={contact} alt="" />
+</div>
+</div>
+</div>    
+ </div>
+    );
+};
+
+export default Contact;
--- src/Shared/HomePage/Homepage.js
+++ src/Shared/HomePage/Homepage.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import About from '../../Component/Pages/About/About';
 import Banner from '../../Component/Pages/Banner/Banner';
+import Contact from '../../Component/Pages/Contact/Contact';
 
 const Homepage = () => {
     return (
         <div>
             <Banner></Banner>
             <About></About>
+            <Contact></Contact>
         </div>
     );
 };
