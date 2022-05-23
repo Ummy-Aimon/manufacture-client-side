0d476ff317ff45784d0f089366ea46aed26edff1
Ummy-Aimon <ummyatia@gmail.com>
2022-05-23 14:04:17 +0600

    login added

--- src/Component/Login/Login.js
+++ src/Component/Login/Login.js
@@ -1,11 +1,124 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Button, Form } from 'react-bootstrap';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
+import {FcGoogle} from 'react-icons/fc'
+import { useSendPasswordResetEmail, useSignInWithEmailAndPassword, useSignInWithGoogle } from 'react-firebase-hooks/auth';
+import { ToastContainer, toast } from 'react-toastify';
+  import 'react-toastify/dist/ReactToastify.css';
+import './Login.css'
+import Loading from '../Loading/Loading';
+import auth from '../../firebase-init';
+import { Divider } from 'react-daisyui';
 
 const Login = () => {
+    const[email,setEmail] = useState('')
+    const[password,setPassword] = useState('')
+    const [signInWithGoogle, user1, loading1, error1] = useSignInWithGoogle(auth);
+    const [sendPasswordResetEmail, loading2, error2] = useSendPasswordResetEmail(
+        auth );
+      
+const [signInWithEmailAndPassword,user,loading,error] = useSignInWithEmailAndPassword(auth);
+      const navigate= useNavigate()
+      let location = useLocation();
+ 
+
+  let from = location.state?.from?.pathname || "/";
+
+
+    const handleEmailBlur = (e) =>{
+        setEmail(e.target.value)
+     }
+     
+     const handlePasswordBlur = e =>{
+         setPassword(e.target.value)
+     }
+
+if(user){
+navigate(from, { replace: true })
+}
+if(user1){
+    navigate(from, { replace: true })
+    }
+
+
+     const handleSignIN =(e) =>{
+        e.preventDefault()
+        if (error) {
+            return   
+          }
+          if (loading) {
+            return 
+          }
+
+          if (error1) {
+            return   
+          }
+          if (loading1) {
+            return 
+          }
+          if (error2) {
+            return   
+          }
+          if (loading2) {
+            return 
+          }
+
+        signInWithEmailAndPassword(email,password)
+     }
     return (
-        <div>
-            <h2>Login</h2>
+        <div className="login">
+       <div  className="title-text">
+              <h1 className=" text-warning fw-bold mt-5 ">Please Login</h1>
+              </div>
+           <Form onSubmit={handleSignIN} className="w-50 m-auto">     
+  <Form.Group className="mb-3" controlId="formGroupEmail">
+    <Form.Label className="fw-bold">Email address</Form.Label>
+    <Form.Control onBlur={ handleEmailBlur} type="email" placeholder="Enter email" required/>
+  </Form.Group>
+  <Form.Group className="mb-3" controlId="formGroupPassword">
+    <Form.Label className="fw-bold">Password</Form.Label>
+    <Form.Control onBlur={handlePasswordBlur} type="password" placeholder="Password" required/>
+    <Button onClick={async () => {
+      if(email){
+        await sendPasswordResetEmail(email);
+        toast('Sent email');
+      }
+      else{
+        toast('Please Enter Your Email')
+      }
+         
+        }} variant="link" type="submit">Forget password?</Button>
+         <ToastContainer />
+    <p className="text-danger">{error2?.message}</p>
+ {
+     loading2 && <Loading></Loading>
+ }
+
+  </Form.Group>
+  
+ <p className="text-danger">{error?.message}</p>
+ {
+     loading && <Loading></Loading>
+ }
+    
+  <p className=" fw-bold"> 
+  Sign Up For Painting Company? <Link className="text-secondary fw-bold text-decoration-none" to="/signup">Create an account</Link>
+</p>
+  <Button type="submit" className="w-100 fw-bold" variant="warning">Login</Button>
+  <br></br> <br></br>
+  <div className="text-center">OR</div>
+  <Button onClick={() => signInWithGoogle()} type= "submit" className="button"> 
+  <FcGoogle> </FcGoogle> Continue With Google </Button>
+  <p className="text-danger">{error1?.message}</p>
+ {
+     loading1 && <Loading></Loading>
+ }
+</Form>
+
         </div>
     );
 };
 
 export default Login;
+
+
--- src/Component/SignUp/SignUp.js
+++ src/Component/SignUp/SignUp.js
@@ -1,10 +1,95 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Button, Form } from 'react-bootstrap';
+import { Link, useNavigate } from 'react-router-dom';
+import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import './SignUp.css'
+import auth from '../../firebase-init';
+import Loading from '../Loading/Loading';
 
 const SignUp = () => {
+   const [name,setName]= useState('')
+   const [email,setEmail]= useState('')
+   const [password,setPassword]= useState('')
+   const [confirmpassword,setConfirmPassword]= useState('')
+   const [error,setError]= useState('')
+  const  navigate= useNavigate()
+  const [
+    createUserWithEmailAndPassword,
+    user,
+    error1,
+    loading ]=
+  useCreateUserWithEmailAndPassword(auth);
+
+  if(error1){
+      return
+  }
+if(loading){
+return
+}
+
+if(user){
+    navigate('/')
+ }
+
+//   form even blur
+
+const handleNameBlur = (e)=>{
+setName(e.target.value)
+}
+const handleEmailBlur = (e)=>{
+    setEmail(e.target.value)
+    }
+    const handlePaswordBlur = (e)=>{
+        setPassword(e.target.value)
+        }
+        const handleConfirmPasswordBlur = (e)=>{
+            setConfirmPassword(e.target.value)
+            }
+            const handleCreateUser = (e)=>{
+                e.preventDefault()
+                if(password !== confirmpassword){
+                    setError('Your password did not match')
+                    return
+                    }
+                    if(password.length <6){
+                        setError('Password must be 6 character')
+                        return;
+                    }
+                
+                createUserWithEmailAndPassword(email, password)
+            }
     return (
-        <div>
-            <h2>signup</h2>
-        </div>
+        <div className="info">
+           <div  className="title-text">
+              <h1 className=" text-warning fw-bold mt-5 ">Please Register</h1>
+              </div>
+              <Form onSubmit={handleCreateUser} className="m-auto w-50 mt-3">
+              <Form.Group className="mb-3" controlId="formBasicPassword">
+    <Form.Control onBlur={handleNameBlur} type="text" placeholder="Enter Your Name" required />
+  </Form.Group>          
+  <Form.Group className="mb-3" controlId="formBasicEmail">
+    <Form.Control onBlur={ handleEmailBlur} type="email" placeholder="Enter Your email" required />
+    <Form.Text className="text-muted">
+      We'll never share your email with anyone else.
+    </Form.Text>
+  </Form.Group>
+  <Form.Group className="mb-3" controlId="formBasicPassword">
+    <Form.Control onBlur={handlePaswordBlur} type="password" placeholder="Password" required />
+  </Form.Group>
+  <Form.Group className="mb-3" controlId="formBasicPassword">
+    <Form.Control onBlur={handleConfirmPasswordBlur} type="password" placeholder=" Comfirm Password" required />
+  </Form.Group>
+  <p className="text-danger">{error}</p>
+  {
+loading && <Loading></Loading>
+} 
+  <p className="fw-bold text-secondary">Already have a account ? <Link className="text-decoration-none text-secondary" to="/login">Login</Link></p>
+  <Button className="fw-bold w-100" variant="warning" type="submit">
+    Register
+  </Button>
+  <p className="text-danger">{error1.message}</p>
+</Form>
+ </div>
     );
 };
 
